40ca221e6f688b983b76f6e02527aaecf0fbe2d7
Hoff <haffedmcnair@gmail.com>
2018-09-06 16:27:17 -0700

    got a camera and player facing reticle

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,26 +30,55 @@ var _this //put the game reference in _this for ease of use
 
 function preload () //preload occurs prior to the scene(game) being instantiated
 {
-    this.load.image('player','assets/CharacterSprites/Player.png')
+    this.load.image('pointer', 'assets/CharacterSprites/pointer.png')
+    this.load.image('player','assets/CharacterSprites/Player_arrow.png')
     this.load.image('enemy','assets/EnemySprites/Enemy.png')
-    this.load.image('playerAttack','assets/CharacterSprites/Attack.png')
+    this.load.image('playerAttack','assets/CharacterSprites/attack-tri.png')
 }
 
 function create () //Occurs when the scene is instantiated
 {
     _this = this
 
-    player = this.physics.add.sprite(160,120,'player')
+    //sprites and background
+    player = this.physics.add.sprite(160,120,'player');
     enemies = this.physics.add.group()
+    reticle = this.physics.add.sprite(180, 120, 'pointer');
+
+        //camera
+        this.cameras.main.setSize(400, 300);
+        this.cameras.main.startFollow(player);
+    
 
     playerEnemyOverlap = this.physics.add.overlap(enemies,player,hitByEnemy)
     cursors = this.input.keyboard.createCursorKeys() //Assigns the input keys. Default is the directional arrows.
+    
     enemy = enemies.create(40,40, 'enemy')
 
     this.input.keyboard.on('keydown_SPACE', function(){
         //On space keydown 'attack'
         attack()
     })
+
+        // Locks pointer on mousedown
+        game.canvas.addEventListener('mousedown', function () {
+            game.input.mouse.requestPointerLock();
+        });
+    
+        // Exit pointer lock when Q or escape (by default) is pressed.
+        this.input.keyboard.on('keydown_Q', function (event) {
+            if (game.input.mouse.locked)
+                game.input.mouse.releasePointerLock();
+        }, 0, this);
+    
+        // Move reticle upon locked pointer move
+        this.input.on('pointermove', function (pointer) {
+            if (this.input.mouse.locked)
+            {
+                reticle.x += pointer.movementX;
+                reticle.y += pointer.movementY;
+            }
+        }, this);
 }
 
 function update () //Update is called every frame
@@ -76,6 +105,18 @@ function update () //Update is called every frame
     else{
         player.setVelocityY(0);
     }
+
+    //player faces reticle
+    player.rotation = Phaser.Math.Angle.Between(player.x, player.y, reticle.x, reticle.y);
+
+    // Makes reticle move with player
+    reticle.body.velocity.x = player.body.velocity.x;
+    reticle.body.velocity.y = player.body.velocity.y;
+
+    // Constrain position of reticle
+    constrainReticle(reticle);
+
+    constrainVelocity(reticle);
 }
 
 function attack(player){ // Called when the player presses spacebar
@@ -97,4 +138,43 @@ function hitByEnemy(player, enemy){
         player.setTint(0xffffff) 
         playerEnemyOverlap = _this.physics.add.overlap(enemies,player,hitByEnemy)
     },1000)
+}
+
+// Ensures sprite speed doesnt exceed maxVelocity while update is called
+function constrainVelocity(sprite, maxVelocity)
+{
+    if (!sprite || !sprite.body)
+      return;
+
+    var angle, currVelocitySqr, vx, vy;
+    vx = sprite.body.velocity.x;
+    vy = sprite.body.velocity.y;
+    currVelocitySqr = vx * vx + vy * vy;
+
+    if (currVelocitySqr > maxVelocity * maxVelocity)
+    {
+        angle = Math.atan2(vy, vx);
+        vx = Math.cos(angle) * maxVelocity;
+        vy = Math.sin(angle) * maxVelocity;
+        sprite.body.velocity.x = vx;
+        sprite.body.velocity.y = vy;
+    }
+}
+
+// Ensures reticle does not move offscreen relative to player
+function constrainReticle(reticle)
+{
+    var distX = reticle.x-player.x; // X distance between player & reticle
+    var distY = reticle.y-player.y; // Y distance between player & reticle
+
+    // Ensures reticle cannot be moved offscreen
+    if (distX > 400)
+        reticle.x = player.x+400;
+    else if (distX < -400)
+        reticle.x = player.x-400;
+
+    if (distY > 300)
+        reticle.y = player.y+300;
+    else if (distY < -300)
+        reticle.y = player.y-300;
 }
